c64c7f7c02a1e0b14c1e8e99dda8d05baff859db
Sharon1118 <sharon98129@gmail.com>
2023-01-10 14:00:01 +0800

    homework3

---
+++ arcd/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/sharon/homework3/arcd
+# Build directory: /home/sharon/homework3/arcd
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("arcd")
+subdirs("examples")
+subdirs("tests")
---
+++ arcd/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sharon/homework3/arcd/tests
+# Build directory: /home/sharon/homework3/arcd/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(codec_tests "/home/sharon/homework3/arcd/tests/codec_tests")
+set_tests_properties(codec_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/sharon/homework3/arcd/tests/CMakeLists.txt;10;add_test;/home/sharon/homework3/arcd/tests/CMakeLists.txt;0;")
